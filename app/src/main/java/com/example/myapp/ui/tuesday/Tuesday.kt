6e6ee6296aba75079df7148ba47167c5a3268d17
Knight-Night-666 <rprakhar14@gmail.com>
2022-02-04 23:42:13 +0530

    Added the backend part, this is the final working project
    UI changes required. Basic functionality now complete

--- app/src/main/java/com/example/myapp/ui/tuesday/Tuesday.kt
+++ app/src/main/java/com/example/myapp/ui/tuesday/Tuesday.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapp.databinding.FragmentTuesdayBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Tuesday : Fragment() {
 
     private lateinit var galleryViewModel: TuesdayViewModel
     private var _binding: FragmentTuesdayBinding? = null
-
+    lateinit var db: DatabaseReference
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -27,14 +29,68 @@ class Tuesday : Fragment() {
 
         _binding = FragmentTuesdayBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        displayStuff()
         /*val textView: TextView = binding.textGallery
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })*/
         return root
     }
+    fun displayStuff()
+    {
+        db= FirebaseDatabase.getInstance().getReference("Menu")
+        db.child("tuesday").child("breakfast").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
 
+                binding.textView3.text=item1.toString()
+                binding.textView4.text=item2.toString()
+                binding.textView5.text=item3.toString()
+                binding.textView6.text=item4.toString()
+                binding.textView7.text=item5.toString()
+
+            }
+        }
+        db.child("tuesday").child("lunch").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
+
+                binding.textView14.text=item1.toString()
+                binding.textView16.text=item2.toString()
+                binding.textView17.text=item3.toString()
+                binding.textView18.text=item4.toString()
+                binding.textView19.text=item5.toString()
+
+            }
+        }
+        db.child("tuesday").child("dinner").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
+
+                binding.textView27.text=item1.toString()
+                binding.textView28.text=item2.toString()
+                binding.textView29.text=item3.toString()
+                binding.textView30.text=item4.toString()
+                binding.textView31.text=item5.toString()
+
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/example/myapp/ui/wednesday/Wednesday.kt
+++ app/src/main/java/com/example/myapp/ui/wednesday/Wednesday.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapp.databinding.FragmentWednesdayBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Wednesday : Fragment() {
 
     private lateinit var slideshowViewModel: WednesdayViewModel
     private var _binding: FragmentWednesdayBinding? = null
-
+    lateinit var db: DatabaseReference
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -27,14 +29,68 @@ class Wednesday : Fragment() {
 
         _binding = FragmentWednesdayBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        displayStuff()
         /*val textView: TextView = binding.textSlideshow
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })*/
         return root
     }
+    fun displayStuff()
+    {
+        db= FirebaseDatabase.getInstance().getReference("Menu")
+        db.child("wednesday").child("breakfast").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
 
+                binding.textView3.text=item1.toString()
+                binding.textView4.text=item2.toString()
+                binding.textView5.text=item3.toString()
+                binding.textView6.text=item4.toString()
+                binding.textView7.text=item5.toString()
+
+            }
+        }
+        db.child("wednesday").child("lunch").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
+
+                binding.textView14.text=item1.toString()
+                binding.textView16.text=item2.toString()
+                binding.textView17.text=item3.toString()
+                binding.textView18.text=item4.toString()
+                binding.textView19.text=item5.toString()
+
+            }
+        }
+        db.child("wednesday").child("dinner").get().addOnSuccessListener {
+            if(it.exists())
+            {
+                val item1 = it.child("item_1").value
+                val item2 = it.child("item_2").value
+                val item3 = it.child("item_3").value
+                val item4 = it.child("item_4").value
+                val item5 = it.child("item_5").value
+
+                binding.textView27.text=item1.toString()
+                binding.textView28.text=item2.toString()
+                binding.textView29.text=item3.toString()
+                binding.textView30.text=item4.toString()
+                binding.textView31.text=item5.toString()
+
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
